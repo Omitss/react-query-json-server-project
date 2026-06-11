9bfd88df7fe5453fa424f2f031004b40a3320e52
oomits <omits118@gmail.com>
2026-06-11 18:49:40 +0900

    deployment5

--- frontend/src/components/home/Dashboard.jsx
+++ frontend/src/components/home/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { useDashBoard } from "../../store/hooks/useDashboard";
+import { useDashBoard } from "../../store/hooks/useDashBoard";
 
 import {
   Chart as ChartJS,
